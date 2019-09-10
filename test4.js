552805aa430690aef2c882b9cf7a41864288e258
herbstar14 <54959136+herbstar14@users.noreply.github.com>
2019-09-10 15:07:34 -0500

    Add files via upload

---
+++ test4.js
@@ -0,0 +1,35 @@
+var department = "cs"
+//basically the point of this file is to show that
+//you can have objects inside objects with functions
+//insde those objects. be sure u remember where the "levels" are
+//dont want to do something out of "scope" 
+var obj = {
+    name: "Andrew", //sometimes you need to use double quotes around things "name": "andrew" 
+    job: "student", 
+    school: "uww", //these3 commas are needed to keep things flowing 
+    
+    anotherobj:{
+        firstname: "Andrew",
+        lastname: "herbst",
+        age: 21,
+        id_num: "1948576",
+    },//end of "anotherobj"
+        
+
+     etc: function(){
+         var returnvalue = this.anotherobj.firstname + " " + this.anotherobj.lastname
+         return returnvalue
+      }//end inner function
+
+
+
+    } //end obj
+
+obj.anotherobj.lastname = "herbst"
+//below is us calling different parts of this weird object we just made 
+console.log(obj.name)
+console.log(obj.etc())
+
+
+
+
